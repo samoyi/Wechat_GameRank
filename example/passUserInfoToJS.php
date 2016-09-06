e5157125ec30ab67cd97047f5543cd806b462aa1
FuncaIDT <info@funca.com.cn>
2016-09-06 17:33:40 +0800

    delete example

--- example/passUserInfoToJS.php
+++
@@ -1,5 +0,0 @@
-<?php
-    echo 'sOpenID = ' . json_encode( $userInfo['openid'] ) . ';';
-    echo 'sNickname = ' . json_encode( $userInfo['nickname'] ) . ';';
-    echo 'sHeadimgurl = ' . json_encode( $userInfo['headimgurl'] ) . ';';
-?>
