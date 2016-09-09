e89d16b0388675ccd826ea067654e5bb586811a8
FuncaIDT <info@funca.com.cn>
2016-09-09 10:04:16 +0800

    添加如果不是从授权页跳转过来的则先跳转至授权页的功能

--- index.php
+++ index.php
@@ -4,21 +4,24 @@
 <meta charset="utf-8">
 <title>title</title>
 </head>
-<body>
-
 <?php
 	require_once('gameRank/main.php');
 ?>
-
-<table id="rank_table"></table>
-
-
-</body>
 <script>
 	<?php
 	    require_once('gameRank/passUserInfoToJS.php');
 	?>
+	if( !sOpenID ) // 如果不是从授权页来的，则跳转到授权页
+	{
+		location.replace("运行createTable.php时生成的授权页地址");
+	}
 </script>
+<body>
+
+<table id="rank_table"></table>
+
+</body>
+
 
 <script src="gameRank/gameRank.js"></script>
 
